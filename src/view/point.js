ece149d68a90e98133d5a21de7582af755e47b16
Зинченко Василий <zinchenkovasiliyserg@gmail.com>
2020-12-02 21:25:51 +0500

    Добавлены моки

--- src/view/point.js
+++ src/view/point.js
@@ -1,29 +1,50 @@
-export const createPoint = () => {
+import dayjs from 'dayjs';
+
+const createOffersElement = ({title, price}) =>{
+  return `<li class="event__offer">
+  <span class="event__offer-title">${title}</span>
+  &plus;&euro;&nbsp;
+  <span class="event__offer-price">${price}</span>
+  </li>`;
+};
+
+export const createPoint = (point) => {
+  const {beginDate, endDate, type, city, price, offers} = point;
+  const date = dayjs(beginDate).format(`MMM DD`);
+  const fullDate = dayjs(beginDate).format(`YYYY-MM-DD`);
+  const startTime = dayjs(beginDate).format(`HH:mm`);
+  const endTime = dayjs(endDate).format(`HH:mm`);
+  const startTimeFull = dayjs(beginDate).format(`YYYY-MM-DDTHH:mm`);
+  const endTimeFull = dayjs(endDate).format(`YYYY-MM-DDTHH:mm`);
+  const durationTime = dayjs(startTimeFull).diff(dayjs(endTimeFull), `minute`);
+
+  let offersList = ``;
+
+  for (let i = 0; i < offers.length; i++) {
+    offersList += createOffersElement(offers[i]);
+  }
+
   return ` <li class="trip-events__item">
   <div class="event">
-    <time class="event__date" datetime="2019-03-18">MAR 18</time>
+    <time class="event__date" datetime="${fullDate}">${date}</time>
     <div class="event__type">
       <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
     </div>
-    <h3 class="event__title">Taxi Amsterdam</h3>
+    <h3 class="event__title">${type} ${city}</h3>
     <div class="event__schedule">
       <p class="event__time">
-        <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+        <time class="event__start-time" datetime="${startTimeFull}">${startTime}</time>
         &mdash;
-        <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+        <time class="event__end-time" datetime="${endTimeFull}">${endTime}</time>
       </p>
-      <p class="event__duration">30M</p>
+      <p class="event__duration">${durationTime}M</p>
     </div>
     <p class="event__price">
-      &euro;&nbsp;<span class="event__price-value">20</span>
+      &euro;&nbsp;<span class="event__price-value">${price}</span>
     </p>
     <h4 class="visually-hidden">Offers:</h4>
     <ul class="event__selected-offers">
-      <li class="event__offer">
-        <span class="event__offer-title">Order Uber</span>
-        &plus;&euro;&nbsp;
-        <span class="event__offer-price">20</span>
-      </li>
+      ${offersList}
     </ul>
     <button class="event__favorite-btn event__favorite-btn--active" type="button">
       <span class="visually-hidden">Add to favorite</span>
