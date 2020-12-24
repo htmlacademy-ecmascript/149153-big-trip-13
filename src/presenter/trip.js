635961d46cb7446fff6216585e797c8fca9d6ed6
Зинченко Василий <zinchenkovasiliyserg@gmail.com>
2020-12-24 16:52:10 +0500

    Добавлена сортировка

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,7 +1,7 @@
 import NoPointsView from '../view/noPoints';
 import ContentView from '../view/content';
 import PointSortView from '../view/sort';
-import {render, RenderPosition, POINT_COUNT, updateItem} from '../utils';
+import {render, RenderPosition, POINT_COUNT, updateItem, SortType, sortDate, sortPrice} from '../utils';
 import PointPresenter from '../presenter/point';
 
 
@@ -9,6 +9,7 @@ export default class Trip {
   constructor(routeContainer) {
     this._routeContainer = routeContainer;
     this._pointPresenter = {};
+    this._currentSortType = SortType.DAY;
 
     this._noPointsComponent = new NoPointsView();
     this._contentComponent = new ContentView();
@@ -16,9 +17,12 @@ export default class Trip {
 
     this._handlePointChange = this._handlePointChange.bind(this);
     this._handleModeChange = this._handleModeChange.bind(this);
+    this._handlerSortTypeChange = this._handlerSortTypeChange.bind(this);
   }
   init(points) {
     this._routePoints = points;
+    this._sourcePoints = points;
+
     render(this._routeContainer, this._contentComponent, RenderPosition.BEFOREND);
     if (points.length) {
       this._renderRoute();
@@ -28,6 +32,7 @@ export default class Trip {
   }
   _renderSort() {
     render(this._routeContainer.firstElementChild, this._pointSortComponent, RenderPosition.AFTERBEGIN);
+    this._pointSortComponent.setSortTypeChangeHandler(this._handlerSortTypeChange);
   }
   _renderPoint(point) {
     const pointPresenter = new PointPresenter(this._contentComponent, this._handlePointChange, this._handleModeChange);
@@ -57,5 +62,32 @@ export default class Trip {
     Object
       .values(this._pointPresenter)
       .forEach((presenter) => presenter.resetView());
+  }
+
+  _sortPoints(sortType) {
+    switch (sortType) {
+      case SortType.TIME:
+        this._routePoints.sort();
+        break;
+      case SortType.PRICE:
+        this._routePoints.sort(sortPrice);
+        break;
+      case SortType.DAY:
+        this._routePoints.sort(sortDate);
+        break;
+      default:
+        this._routePoints = this._sourcePoints;
+    }
+    this._currentSortType = sortType;
+  }
+
+  _handlerSortTypeChange(sortType) {
+    if (this._currentSortType === sortType) {
+      return;
+    }
+
+    this._sortPoints(sortType);
+    this._clearPointList();
+    this._renderRoute();
   }
 }
